f07da4b8184a2d81eb5086efe103d648532766b4
mtaniuchi <mtaniuchi@gmail.com>
2014-10-23 17:46:06 +0900

    executable(zip) added.

--- ShowFileVersionInfo/ShowFileVersionInfo/Properties/AssemblyInfo.cs
+++ ShowFileVersionInfo/ShowFileVersionInfo/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("ShowFileVersionInfo")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("ShowFileVersionInfo")]
+[assembly: AssemblyConfiguration("ShowFileVersionInfo")]
+[assembly: AssemblyCompany("mtaniuchi")]
 [assembly: AssemblyProduct("ShowFileVersionInfo")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © mtaniuchi 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
